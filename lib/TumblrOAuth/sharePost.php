1bb3aa21900f656036c836a50212a04f3c4b9ed0
Geoffrey <geoff@offlinela.com>
2013-09-18 12:16:55 -0700

    shareBalls not yet

--- lib/TumblrOAuth/sharePost.php
+++ lib/TumblrOAuth/sharePost.php
@@ -15,5 +15,5 @@ $userinfo = $tum_oauth->get('http://api.tumblr.com/v2/user/info');
 //$newPost = $tum_oauth->post('http://api.tumblr.com/v2/blog/'.$userinfo['name'].'.tumblr.com/post',$params);
 
 //echo json_encode($userinfo);
-echo $userinfo['response']['user'][0]['name'];
+echo $userinfo['response']['user']['blogs'][0]['name'];
 ?>
