0fb99a7e8e97bae74a46baa5a15321a98dfc8739
Geoffrey <geoff@offlinela.com>
2013-09-26 11:33:59 -0700

    updated script

--- lib/TumblrOAuth/getPosts.php
+++ lib/TumblrOAuth/getPosts.php
@@ -13,8 +13,7 @@ $userinfo = $tum_oauth->get('http://api.tumblr.com/v2/user/info');
 
 $newPost = $tum_oauth->get($userinfo->response->user->url, $params);
 
-//$url = "http://api.tumblr.com/v2/blog/".$userinfo->response->user->name.".tumblr.com/posts/photo?&limit=20&offset=20&api_key=CVzhkkyjfjeKsGeDuOflRGddP14Y3jzDLGUi0QnHTfvnAF010D";
-$url = "http://api.tumblr.com/v2/blog/monk3ypoop.tumblr.com/posts/photo?&limit=20&offset=20&api_key=CVzhkkyjfjeKsGeDuOflRGddP14Y3jzDLGUi0QnHTfvnAF010D";
+$url = "http://api.tumblr.com/v2/blog/".$userinfo->response->user->name.".tumblr.com/posts/photo?&limit=20&offset=20&api_key=CVzhkkyjfjeKsGeDuOflRGddP14Y3jzDLGUi0QnHTfvnAF010D";
 
 $result = file_get_contents($url);
 echo $result;
