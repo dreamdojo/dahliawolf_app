a2c9bb88c651048433ea55d6eab6e89a0631a891
Geoffrey <geoff@offlinela.com>
2013-10-02 14:11:52 -0700

    redirect

--- blocks/filter.php
+++ blocks/filter.php
@@ -2,18 +2,18 @@
     <div class="filter-wrap">
         Sort Inspirations By:
         <span class="">
-            <a id="filterNewest" class="sort-option <?= ($_GET['sort'] == 'new' || empty($_GET['sort']) ? 'filter-select' : '') ?>" href="/spine?sort=new">newest</a>
+            <a id="filterNewest" class="sort-option <?= ($_GET['sort'] == 'new' || empty($_GET['sort']) ? 'filter-select' : '') ?>" href="/vote?sort=new">newest</a>
         </span> /
         <span class="">
-            <a id="filterTrending" class="sort-option <?= ($_GET['sort'] == 'hot' ? 'filter-select' : '') ?>" href="/spine?sort=hot"> trending</a>
+            <a id="filterTrending" class="sort-option <?= ($_GET['sort'] == 'hot' ? 'filter-select' : '') ?>" href="/vote?sort=hot"> trending</a>
         </span>
         <? if (IS_LOGGED_IN): ?>
              / <span class="">
                     <a id="filterFollowing" class="sort-option <?= ($_GET['sort'] == 'following' ? 'filter-select' : '') ?>" href="/spine?sort=following"> following</a>
             </span>
         <? endif ?>
-        <span style="float: right;width: 150px;margin-right: -3px;">View: <span class=""><a id="selectSpine" class="sort-option" href="/spine">two</a></span> /
-        <span class=""><a id="selectGrid" class="sort-option filter-select" href="/grid"> three</a></span>
+        <span style="float: right;width: 150px;margin-right: -3px;">View: <span class=""><a id="selectSpine" class="sort-option" href="/vote">two</a></span> /
+        <span class=""><a id="selectGrid" class="sort-option filter-select" href="/vote"> three</a></span>
         </span>
     </div>
 </div>
