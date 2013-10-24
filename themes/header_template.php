645934e43ed0b3d3f9fb49c098991d2f03535bca
Geoffrey <geoff@offlinela.com>
2013-10-24 16:30:24 -0700

    change default to spine

--- themes/header_template.php
+++ themes/header_template.php
@@ -101,7 +101,7 @@ $(document).ready(function()
                 <a href="/<?= $_SESSION['user']['username'] ?>"><li style="border-top: none;">Profile</li></a>
                 <a href="/<?= $_SESSION['user']['username'] ?>?dashboard=true"><li>Dashboard</li></a>
                 <a href="/activity"><li>Activity</li></a>
-                <a href="/invite"><li>Invite</li></a>
+                <a href="/invite"><li>Grow My Clique</li></a>
                 <!--<a href="/shop/my-wishlist"><li>Wishlist</li></a>-->
                 <a href="/pinit"><li>Inspire Tool</li></a>
                 <a href="/account/settings"><li>Settings</li></a>
