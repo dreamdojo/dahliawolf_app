4cd6a1a65ea19d813a6a0fba2f13842cdcb57956
Geoffrey <geoff@offlinela.com>
2013-10-28 18:39:25 -0700

    add paxton to vip and redeem mail list

--- action/viprequest.php
+++ action/viprequest.php
@@ -1,5 +1,5 @@
 <?php
-$to = "vip@dahliawolf.com";
+$to = "vip@dahliawolf.com, paxton@dahliawolf.com";
 $subject = "user submitted vip request";
 $message = "VIP ME FOO! ".$_POST['user'];
 $from = "admin@dahliawolf.com";
