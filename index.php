8f1c57f4ed1096b1cf049ad28db6f21220b8e998
Geoffrey <geoff@offlinela.com>
2013-10-29 11:45:54 -0700

    we landed on the moon

--- index.php
+++ index.php
@@ -75,8 +75,8 @@ if(isset($_COOKIE["dahliaUser"]) && isset($_COOKIE["token"])){
 
     .slide{ height:100%; width:100%; position:absolute; left:0px; top:0px;background-size: auto 100%;background-repeat: no-repeat;background-position: center; background-size: cover;}
     .slide img{min-width:100%; min-height:100%; }
-    .theBox{padding-bottom: 3%; width: 40%; height: auto; position: fixed; top: 41px;left: 60px; z-index: 1;font-size: 13px;color: #C5C9CC; min-width:250px;  max-width:400px; overflow: hidden; font:Arial, Helvetica, sans-serif; text-align: center;}
-    .theBox .header{position: relative;z-index: 1;margin-top: 7%; width: 80%;}
+    .theBox{height: auto;position: fixed;top: 35px;z-index: 1;font-size: 13px;text-align: center;left: 35px;}
+    .theBox .header{position: relative;z-index: 1;}
     .strange-clouds{opacity: 0.5;background-color: #000; position:absolute; height:100%; width:100%; top:0px; left:0px;z-index: -1;}
 
     #slideshow-frame{width: 100%;height: 100%;position: fixed;left: 0px;top: 0px;overflow: hidden;}
@@ -86,34 +86,21 @@ if(isset($_COOKIE["dahliaUser"]) && isset($_COOKIE["token"])){
     .nativeBox li{width: 45%;text-align: center;float: left;}
     .nativeBox li img{width: 66%;}
     .lbeezy{width: 80%; border-top:#474747 thin solid; padding-bottom: 15%; margin: 0px auto;}
+    .enterTheDragon{position: fixed; right: 35px; top: 35px; color: #fff;font-size: 18px;font-family: futura;}
 </style>
 
 <body>
 
 <div id="slideshow-frame">
-    <video style="min-width: 100%; min-height: 100%;" autoplay loop>
+    <video id="theVideo" style="min-width: 100%; min-height: 100%;" autoplay>
         <source src="http://dev.dahliawolf.com/images/video/story.mp4" type="video/mp4">
         <source src="http://dev.dahliawolf.com/images/video/story.ogv" type="video/ogg">
     </video>
 </div>
+<div class="enterTheDragon"><a href="/home">ENTER SITE</a></div>
 
 <div class="theBox">
     <img class="header loggle-toggle" src="/images/homeTitle.png" />
-    <div class="boxCTA"><a href="/home">ENTER SITE</a></div>
-    <div class="lbeezy"></div>
-    <ul class="nativeBox">
-        <li>
-            <a href="https://play.google.com/store/apps/details?id=com.zyonnetworks.dahliawolf2&hl=en" target="_blank">
-                <img src="/images/droid_trans_bg.png">
-            </a>
-        </li>
-        <li>
-            <a href="https://itunes.apple.com/us/app/dahlia-wolf-fashion/id718253685?ls=1&mt=8" target="_blank">
-                <img src="/images/ios_trans_bg.png">
-            </a>
-        </li>
-    </ul>
-    <div class="strange-clouds"></div>
 </div>
 
 </body>
@@ -124,6 +111,10 @@ if(isset($_COOKIE["dahliaUser"]) && isset($_COOKIE["token"])){
 
         $('#slideshow-frame video').on('play', centerVideo);
         $(window).resize(centerVideo);
+
+        $('#theVideo').bind('ended', function() {
+            document.location = '/home';
+        });
     });
 
 function centerVideo() {
