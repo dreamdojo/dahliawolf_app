f4c88defbcf8233ecd17f11ea0bbfb410dccb08a
Javier Dorado - github <javier@offlinela.com>
2013-10-30 16:10:49 -0700

    adding blog before Grow my clique

--- themes/header_template.php
+++ themes/header_template.php
@@ -113,6 +113,7 @@ $(document).ready(function()
                     <a href="/<?= $_SESSION['user']['username'] ?>"><li style="border-top: none;">Profile</li></a>
                     <a href="/<?= $_SESSION['user']['username'] ?>?dashboard=true"><li>Dashboard</li></a>
                     <a href="/activity"><li>Activity</li></a>
+                    <a href="http://blog.dahliawolf.com/"><li>blog</li></a>
                     <a href="/invite"><li>Grow My Clique</li></a>
                     <a href="/account/settings"><li>Settings</li></a>
                     <a href="/shop/my-orders"><li>Orders</li></a>
