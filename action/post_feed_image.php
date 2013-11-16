0ec365fea825e4b58c1052673394d2b096b8dbfc
Javier Dorado - github <javier@offlinela.com>
2013-11-15 19:13:49 -0800

    fix settings.. add ignores for email_content

--- action/post_feed_image.php
+++ action/post_feed_image.php
@@ -26,8 +26,23 @@ if (empty($data['errors'])) {
 		
 		//, 'id' => $_POST['id']
 	);
-	$data = api_call('posting', 'add_post_image', $params, true);
-	
+
+
+
+    // Add to image/posting
+    $bank_images_params = array(
+        'user_id' => $_SESSION['user']['user_id'],
+        'repo_image_id' => $_POST['id'],
+        'use_hmac_check' => '0',
+    );
+
+    $user_bank_images = api_request('posting', 'get_bank_images', $bank_images_params, true);
+    echo "<!-- ".var_export($user_bank_images, true). " -->";
+
+
+    ////
+    $data = api_call('posting', 'add_post_image', $params, true);
+
 	header('Content-Type: application/json');
 	echo json_encode($data['data']);
 }
