c4d11d82284838c6a5f642e439a41b0f989ad9d2
Javier Dorado - github <javier@offlinela.com>
2013-11-18 09:42:34 -0800

    adding limit per day 5post limit

--- action/post_feed_image.php
+++ action/post_feed_image.php
@@ -28,22 +28,34 @@ if (empty($data['errors'])) {
 	);
 
 
+    $calls = array(
+   		'get_bank_images' => array(
+   		    'user_id' => $_SESSION['user']['user_id'],
+   		    'repo_image_id' => $_POST['id'],
+   		    'use_hmac_check' => 0,
+            'limit_per_day' => 1
+        )
+   	);
 
-    // Add to image/posting
-    $bank_images_params = array(
-        'user_id' => $_SESSION['user']['user_id'],
-        'repo_image_id' => $_POST['id'],
-        'use_hmac_check' => '0',
-    );
+    $user_bank_images = api_request('posting', $calls, true);
+    //echo "<!-- ".var_export($user_bank_images, true). " -->";
 
-    $user_bank_images = api_request('posting', 'get_bank_images', $bank_images_params, true);
-    echo "<!-- ".var_export($user_bank_images, true). " -->";
+    if($user_bank_images && $user_bank_images != null && @count($user_bank_images['data']['get_bank_images']['data']) >= 5 )
+    {
+        //sorry pal no mo images fo u!!!
+        echo json_encode( array(
+                            'error' => "Sorry due to high demand we have temporally limited the number of images you can post from the D\W Image Bank.",
+                            'data' => null,
+                            'posting_id' => null
+        ));
+    }else{
 
+        ////post the image
+        $data = api_call('posting', 'add_post_image', $params, true);
 
-    ////
-    $data = api_call('posting', 'add_post_image', $params, true);
+        header('Content-Type: application/json');
+        echo json_encode($data['data']);
+    }
 
-	header('Content-Type: application/json');
-	echo json_encode($data['data']);
 }
 ?>
