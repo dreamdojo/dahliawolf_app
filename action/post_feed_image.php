414ad3742f0b824798715e0d5ef804e0a704086d
Javier Dorado - github <repos@jk07.com>
2013-11-17 21:25:29 -0800

    adding posting 5 limit

--- action/post_feed_image.php
+++ action/post_feed_image.php
@@ -26,9 +26,31 @@ if (empty($data['errors'])) {
 		
 		//, 'id' => $_POST['id']
 	);
-	$data = api_call('posting', 'add_post_image', $params, true);
-	
-	header('Content-Type: application/json');
-	echo json_encode($data['data']);
+
+
+
+    // Add to image/posting
+    $bank_images_params = array(
+        'user_id' => $_SESSION['user']['user_id'],
+        'repo_image_id' => $_POST['id'],
+        'use_hmac_check' => '0',
+    );
+
+    $user_bank_images = api_request('posting', 'get_bank_images', $bank_images_params, true);
+    echo "<!-- ".var_export($user_bank_images, true). " -->";
+
+    if($user_bank_images && $user_bank_images != null && @count($user_bank_images['data']['get_bank_images']['data']) >= 5 )
+    {
+        //sorry pal no mo images fo u!!!
+        echo json_encode(array('error' => "Sorry due to high demand we have temporally limited the number of images you can post from the D\W Image Bank.", 'data' => null));
+    }else{
+
+        ////post the image
+        $data = api_call('posting', 'add_post_image', $params, true);
+
+        header('Content-Type: application/json');
+        echo json_encode($data['data']);
+    }
+
 }
 ?>
