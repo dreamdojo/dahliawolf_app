bbf399ac00caf00a83e91b51193bbb2a2faa47b7
Geoffrey <geoff@offlinela.com>
2013-12-18 16:29:17 -0800

    hashtag

--- blocks/fashiolista.php
+++ blocks/fashiolista.php
@@ -9,6 +9,23 @@
     #fashiolistaPop .members{margin-bottom: 35px;font-size: 13px;}
     #fashiolistaPop .avatar{width: 100px;height: 100px;background-color: #fff;border-radius: 50px;margin: 10px auto;}
     #fashiolistaPop #getStarted{width: 200px;height: 35px;color: #000;background-color: #fff;margin: 0px auto;line-height: 35px;font-size: 17px;margin-top: 40px; cursor: pointer;}
+
+    #contestCongrats{background-image: url("/images/contest-congrats-bg.jpg");
+        background-size: cover;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        width: 780px;
+        height: 500px;
+        position: fixed;
+        margin-top: -250px;
+        margin-left: -390px;
+        top: 50%;
+        left: 50%;
+        z-index: 100000;
+        text-align: center;
+        color: #fff;
+        display: none;}
+
 </style>
 <div id="fashioOverlay" class="overlay"></div>
 <div id="fashiolistaPop">
@@ -25,14 +42,33 @@
     </ul>
     <div id="getStarted">GET STARTED</div>
 </div>
+
+<div id="contestCongrats">
+    <div style="margin-top: 37px;margin-bottom: 120px;"><img src="/images/contest-logos.png"></div>
+    <div><img src="/images/contest-message.png"></div>
+    <div style="margin-top: 127px;"><img id="closePop" src="/images/contest-close-button.png"></div>
+</div>
+
 <script>
     $(function() {
         $('#getStarted').on('click', function() {
             $('#fashiolistaPop').fadeOut(100);
             $('#fashioOverlay').fadeOut(200);
         });
+
         $(document).one('click', '.postButton', function() {
-            console.log('success');
+            $('#contestCongrats').fadeIn(200);
+            $('#fashioOverlay').fadeIn(200).on('click', function() {
+                $(this).fadeOut(200);
+                $('#contestCongrats').fadeOut(200);
+            });
         });
+
+        $('#closePop').on('click', function() {
+            $('#fashioOverlay').fadeOut(200);
+            $('#contestCongrats').fadeOut(200);
+        });
+
+
     });
 </script>
