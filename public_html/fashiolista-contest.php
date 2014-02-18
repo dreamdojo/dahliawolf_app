f75dacf0592f3073461648c405863e070c101a45
justinmavandi <justin.mavandi@gmail.com>
2014-02-18 10:01:37 -0800

    contest updates

--- public_html/fashiolista-contest.php
+++ public_html/fashiolista-contest.php
@@ -13,7 +13,7 @@ include "header.php";
 .row2 ul{ width: 27%; float: left; text-align: center; background-color: #FAFAFA; border: #B8B8B8 thin solid; padding-bottom: 30px;}
 .row2 ul p{font-size: 12px;width: 65%;margin: 0px auto;padding-bottom: 20px;}
 .row2 ul li:last-child{background-color: #ff1176; padding: 10px 10px; font-size: 12px; color: #fff; width: 65%; margin: 0px auto;}
-.transCol h3{width: 100%;margin: 0px auto;margin-top: 15px;font-size: 14px;margin-bottom: 10px;line-height: 25px;}
+.transCol h3{width: 100%;margin: 0px auto;margin-top: 15px;font-size: 14px;margin-bottom: 10px;line-height: 26px;}
 .transCol h1{margin-bottom: 75px;}
 .row2 h1{margin-top: 30px; margin-bottom: 25px;}
 </style>
@@ -22,9 +22,16 @@ include "header.php";
     <div class="goodieGrad"></div>
     <img style="margin-bottom: 30px;" width="850px" src="/images/fashiolista-contest.jpg">
     <h2 style="margin:0px;">Fashiolista Bloggers Battle</h2>
-    <h3 style="margin-bottom: 50px;">1) Visit Fashiolista.com and vote on your favorite bloggers look to decide who will win a Dahlia Wolf collection. <br>2) Post your own inspiration on Dahlia Wolf to win a 5 piece collection of your own!<br>2) Email <a href="mailto:contests@dahliawolf.com"/>contests@dahliawolf.com</a> with your full name and Dahlia Wolf username</h3>
     <a href="http://www.fashiolista.com/iframe_contest/dahlia-wolf-contest/"><h3>Visit the Fashiolista x Dahlia Wolf contest page.<br>CONTEST STARTS FEB. 25TH 2014</h3></a>
-    <h2 style="margin-bottom:20px">BLOGGER LIST...</h2>
+    <h3 style="margin-top: 50px;"><b>Contest Details</b></h3>
+    <h3 style="margin-bottom: 50px;">1) Visit Fashiolista.com and vote on your favorite bloggers look to decide who will win a Dahlia Wolf collection. <br>2) Post your own inspiration on Dahlia Wolf to win a 5 piece collection of your own!<br>2) Email <a href="mailto:contests@dahliawolf.com"/>contests@dahliawolf.com</a> with your full name and Dahlia Wolf username</h3>
+    <h3><b>Prizes</b></h3>
+    <h3>3 bloggers and 1 members will win a chance to design a one of a kind 5 piece Dahlia Wolf collection
+
+    <br><b>Grand Prize:</b> The top blogger and member will win a round trip ticket and 1 night stay to come to Los Angeles, CA to work with the Dahlia Wolf team to create there new 5 piece collecion.</h3>
+
+
+    <h2 style="margin-bottom:20px">Blogger list...</h2>
 
     <a href="/danithorne" style="line-height: 20px; margin-bottom: 10px; font-size: 16px;"><span>Dani Thorne</span>
     <h3 style="line-height: 20px;margin-top:0px; margin-bottom:0px;color: #B6B6B6;">@danithorne</h3>
